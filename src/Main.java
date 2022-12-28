142451f411e837680d59319e774bc360effd76f1
christian.moreira <christian.moreira@tre-rj.jus.br>
2022-12-27 23:22:26 -0300

    Comitando alteração simples

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Teste");
+		System.out.println("Teste");
+		System.out.println("Comitando alteração simples");
 	}
 
 }
